76c0f60e2bf9a359114161335b18f12788e5e513
devswarnali <166713710+devswarnali@users.noreply.github.com>
2024-06-10 03:14:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$('.exp-slide').slick({
+    slidesToShow: "4",
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
+$('.slide').slick({
+    dots: false,
+    arrow: true,
+    fade: false,
+    autoplay: false,
+    autoplaySpeed: 4000,
+});
+
+$('.feedback').slick({
+  dots: false,
+  arrow: true,
+  fade: false,
+  autoplay: false,
+  autoplaySpeed: 4000,
+});
