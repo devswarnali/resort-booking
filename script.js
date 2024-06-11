e6e7f7ef91e634583e5de0a96fdd50b143be0c45
devswarnali <166713710+devswarnali@users.noreply.github.com>
2024-06-11 23:19:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,76 @@
 $('.exp-slide').slick({
-    slidesToShow: "4",
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
 
 $('.slide').slick({
     dots: false,
     arrow: true,
     fade: false,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 4000,
 });
+
+// $('.responsive').slick({
+//   dots: true,
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//   ]
+// });
+				
 
 $('.feedback').slick({
   dots: false,
   arrow: true,
   fade: false,
-  autoplay: false,
+  autoplay: true,
   autoplaySpeed: 4000,
 });
